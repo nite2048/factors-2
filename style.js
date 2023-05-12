21e50210a22b839711baed236180a773aafb4a7d
ABHIROOPKAR <abhiroop.kar2010@gmail.com>
2023-05-12 14:38:20 +0530

    1.9.2: minor fixes

--- style.js
+++ style.js
@@ -27,6 +27,8 @@ function handle_device_change(query) {
     }
 
     on_click(0)
+
+    setInterval(() => {const element = document.getElementById("load"); element.remove()}, 2000);
 }
 
 handle_device_change(media_query)
